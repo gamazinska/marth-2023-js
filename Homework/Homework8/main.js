0a2fe1d5ffcb67fb33633eeb673cdf06d7d97a87
gamazinska <gamazinskaya86@mail.ru>
2023-05-30 15:15:07 +0300

    povtorwork1

--- Homework/Homework8/main.js
+++ Homework/Homework8/main.js
@@ -1,6 +1,6 @@
 // - Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
 // створити пустий масив, наповнити його 10 об'єктами new User(....)
-function User(id, name, surname, email, phone) {
+//function User(id, name, surname, email, phone) {
     this.id = id;
     this.name = name;
     this.surname = surname;
